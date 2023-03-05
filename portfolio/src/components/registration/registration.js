9df15d49c465b3fbc0494d995917003c1677b6c3
Faith Moraa <moraaf770@gmail.com>
2023-03-05 15:55:27 +0300

    Added js file for registration

--- portfolio/src/components/registration/registration.js
+++ portfolio/src/components/registration/registration.js
@@ -1,48 +1,73 @@
-import React from "react"
+import React, {useState} from "react"
 import './registration.css'
+import Image from './img.jpeg'
 
-function Registration (){
+
+function Registration(props){
+    const [ img, setImg] = useState(props.src)
+    const [name, setName] = useState()
+    const [email, setEmail] =useState()
+    const [project, setProject]  = useState()
+    const [skill, setSkill]  = useState()
+
+    const handleImageError = () => {
+        setImg('placeholder.png')
+    }
 
     return(
-        <div className="registration">
-            <div className="r-container">
-                    <div className="r-left">
-                    <p className="r-title">Bio</p>
+        <div className="reg-container">
+            <div className="r-left">
+            
+                <img src={Image} alt="Add Image" onError={handleImageError} className="reg-img" />
 
-
-                 </div>
-            </div>
-
-            <div className="r2-container">
-                <div className="r-right"></div>    
-                <div className="flex-container">
-                    <div className="projects-card">
-                        <p className="project-title">Projects</p>
-                        <p>Project description</p>
-                    </div> 
-
-                    <div className="projects-card">
-                        <p className="project-title">Projects</p>
-                        <p>Project description</p>
-                    </div> 
-
-                    <div className="projects-card">
-                        <p className="project-title">Projects</p>
-                        <p>Project description</p>
-                    </div> 
-
-                    <div className="projects-card">
-                        <p className="project-title">Projects</p>
-                        <p>Project description</p>
-                    </div> 
-                
-
-               
+                <div className="reg-user-det">
+                <p className="reg-name">Name</p>
+                <p className="reg-email">Email</p>
 
                 </div>
+                
+         
+
+            
+                <div>
+                    <p className="reg-title">Add Projects</p>
+                    
+                    <input
+                    type="text" 
+                    onChange={project}
+                    placeholder="Add Project Name"
+                    className="reg-project-name"
+                    /> 
+          
+                <textarea className="reg-project-desc">Add project description</textarea>
+                    
+                 
+                </div>
+
+                <div>
+                    <p className="reg-title">Add Skills</p>
+                    <input
+                    type="text" 
+                    onChange={skill}
+                    placeholder="Add skills"
+                    className="reg-skill-name"
+                    />  
+
+               <textarea className="reg-skill-desc">Add project description</textarea>
+                   
+                </div>
+
+            
+
             </div>
-        </div>        
+            <div className="r-right">
+                <button className="reg-btn">Save Changes</button>
+            </div>
+            
+        </div>
     )
+
 }
+
 
 export default Registration
