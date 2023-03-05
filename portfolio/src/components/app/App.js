29857f2dc658ac188926c11c27d4711d1b767c4a
Faith Moraa <moraaf770@gmail.com>
2023-03-05 23:37:02 +0300

    Changed rl paths in app.js

--- portfolio/src/components/app/App.js
+++ portfolio/src/components/app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css'
-import Login from '../login/Login';
+import Login from '../others/login/Login';
 import Profile from '../my-profile/my-profile';
-import Login2 from '../login2/login2';
+import Login2 from '../others/login2/login2'
 import Landing from '../landing/landing';
-import Home from '../home/home';
+import Home from '../others/home/home';
 import Registration from '../registration/registration';
 import ProjectCard from '../my-profile/profile-card';
 
@@ -16,8 +16,8 @@ function App() {
     //<Login2/>
     //<Landing/>
     //<Home />
-    //<Registration />
-    <Profile />
+    <Registration />
+    //<Profile />
     //<ProjectCard />
   );
 }
